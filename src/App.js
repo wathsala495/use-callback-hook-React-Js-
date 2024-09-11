413286d34f4afc9c2082c850d598a0e4f7a64484
wathsala495 <sumuduwathsala80@gmail.com>
2024-09-11 16:29:09 +0530

    use callback hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 
+
+//use memo eka application eka speed karann bawitha karai(performane wadi karann)
+//use memo-returns a value
+//use calleback=return a function
+//use callback use for to optimize a app(also  memo)
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  const [imageUrl,setImageUrl]=useState('')
+  const [name,setName]=useState('')
+  const [result,setResult]=useState('')
+
+  const myFunction=useCallback(()=> ()=>"amila"
+  ,[])
+  
+  console.log("rerender");
+  console.log(result)
+
+  useEffect(()=>{
+     console.log("use Effect")
+     //console.log(myFunction())
+     setResult(myFunction())
+  },[myFunction])
+ 
+     return (
+    <div >
+     <input type='text' placeholder='Enter image url' onChange={(e)=>setImageUrl(e.target.value)} />
+     <input type='text' placeholder='Enter your name' onChange={(e)=>setName(e.target.value)}/>
+     <input type='text' placeholder='Enter your city' />
+     <input type='text' placeholder='Enter your position' />
+     <button >Set Data</button>
+    
+     <div>
+      <p>{imageUrl}</p>
+       <p>{name}</p>
     </div>
+    </div>
+
+   
   );
 }
 
